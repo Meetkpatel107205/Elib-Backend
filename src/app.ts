db3cc7bfe41c3a0031a0d0ab38151e6736f79e4b
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-11-28 14:27:59 +0530

    Create User Router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import globalErrorHandler from './middlewares/globalErrorHandler.js';
+import userRouter from './user/userRouter.js';
 
 const app = express();
 
@@ -10,6 +11,8 @@ const app = express();
 app.get('/', (req, res, _next) => {
     res.json({message: "Welcome to elib apis"});
 });
+
+app.use('/api/users', userRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
---
+++ src/user/userRouter.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { createUser } from './userController.js';
+
+const userRouter = express.Router();
+
+// Routes :-
+
+userRouter.post('/register', createUser);
+
+export default userRouter;
