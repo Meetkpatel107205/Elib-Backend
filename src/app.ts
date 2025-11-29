820b8cd3864c9bef1083646ccc4305c56a44f27b
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-11-29 15:26:27 +0530

    Add CORS Support

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import express from 'express';
 import globalErrorHandler from './middlewares/globalErrorHandler.js';
 import userRouter from './user/userRouter.js';
 import bookRouter from './book/bookRouter.js';
+import cors from 'cors';
+import { config } from './config/config.js';
 
 const app = express();
+
+app.use(cors({
+    origin: config.frontendDomain,
+})
+);
 
 app.use(express.json());
 
